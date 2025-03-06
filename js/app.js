fbced6a82b491ed6f1b2dfb560524fd628154bce
Luca-Rubino <lucasrubino92@gmail.com>
2025-03-06 14:01:11 +0100

    Create function activeSublistNav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('sono collegato')
+console.log('connected');
+
+
+function activeSublistNav() {
+    if (document.getElementsByClassName('sublist-nav')[0].style.display === 'none') {
+        document.getElementsByClassName('sublist-nav')[0].style.display = 'flex';
+    } else {
+        document.getElementsByClassName('sublist-nav')[0].style.display = 'none';
+    }
+}
